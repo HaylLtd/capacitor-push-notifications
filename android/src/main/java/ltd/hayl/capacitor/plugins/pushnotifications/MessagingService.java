d2e1e8af204f43802a84babb14a0c16d3e251837
Adam Radestock <raddishiow@gmail.com>
2023-03-30 22:48:23 +0100

    chore: initial commit

---
+++ android/src/main/java/ltd/hayl/capacitor/plugins/pushnotifications/MessagingService.java
@@ -0,0 +1,20 @@
+package ltd.hayl.capacitor.plugins.pushnotifications;
+
+import androidx.annotation.NonNull;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        PushNotificationsPlugin.sendRemoteMessage(remoteMessage);
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        PushNotificationsPlugin.onNewToken(s);
+    }
+}
